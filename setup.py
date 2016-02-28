ceef5475c9c663b3cddf6b495b6e27c10c21c311
capJavert <ante.baric3@gmail.com>
2016-02-28 02:39:30 +0100

    init

---
+++ setup.py
@@ -0,0 +1,24 @@
+__author__ = 'Ante Barić @capJavert'
+
+from setuptools import setup
+
+setup(
+    name='python-translate',
+    version='0.1',
+    summary='shell tool',
+    homepage='https://github.com/capJavert/python-translate',
+    author='capJavert',
+    author_email='/',
+    license='MIT',
+    description='Simple command line tool for croatian/english translation',
+    platforms='windows, linux, os x',
+
+    py_modules=['translate'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        translate=translate:main
+    ''',
+)
---
+++ translate.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+__author__ = 'Ante Barić @capJavert'
+
+import click
+import requests
+
+@click.command()
+@click.option('--word', default="", help='Word that you wish to translate.')
+@click.option('--lang', default="en", help='Language in which word is wrote ("en" or "hr"). Default is english language.')
+def main(word, lang):
+    if word=="":
+        print("     You are missing --word param. Use --help for more info.")
+        return
+
+    url = 'http://www.design-ers.net/eh-rjecnik.asp'
+
+    form_data = {
+        'rijec': word,
+        'lang': lang,
+        'Submit': 'Prevedi',
+    }
+
+    response = requests.post(url, data=form_data)
+    response.encoding = 'Windows-1250'
+
+    i = 0;
+    translations = '     translation is not available :('
+    for line in response.text.splitlines():
+        i = i + 1;
+        if (i==115) and (line!='     data-ad-client="ca-pub-9335582685562862"'):
+            translations = line
+
+    print(translations)
+    return
+
+main()
